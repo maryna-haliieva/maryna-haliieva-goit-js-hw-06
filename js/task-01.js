4126b9e94be964287d7cb576febfa1e0cbb507cb
maryna-haliieva <marina.galieva7@gmail.com>
2022-07-07 10:50:04 +0300

    task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,9 @@
 const totalCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${totalCategories.length}`);
-
-constcategoriesArray = [...totalCategories]
+const categoriesArray = [...totalCategories]
   .map(
-    (categories) => `Category^ ${categories.children[0].textContentContent}
-        Elements: ${categories.children[1].lengtgh}`
+    (categories) => `Category: ${categories.children[0].textContent}
+Elements: ${categories.children[1].children.length}`
   )
   .join("\n");
 console.log(categoriesArray);
