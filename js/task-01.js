87a8ccde596b00d61bddcb5f29ab2aefb418a0c0
maryna-haliieva <marina.galieva7@gmail.com>
2022-07-07 11:39:24 +0300

    Update task 01

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,23 @@
-const totalCategories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${totalCategories.length}`);
-const categoriesArray = [...totalCategories]
-  .map(
-    (categories) => `Category: ${categories.children[0].textContent}
-Elements: ${categories.children[1].children.length}`
-  )
-  .join("\n");
-console.log(categoriesArray);
+// const totalCategories = document.querySelectorAll(".item");
+// console.log(`Number of categories: ${totalCategories.length}`);
+// const categoriesArray = [...totalCategories]
+//   .map(
+//     (categories) => `Category: ${categories.children[0].textContent}
+// Elements: ${categories.children[1].children.length}`
+//   )
+//   .join("\n");
+// console.log(categoriesArray);
+// =====================================
+
+const createCategories = document.querySelector("#categories");
+const categoriesItem = [...createCategories.children];
+console.log(categoriesItem);
+console.log(`Number of categories: ${categoriesItem.length}`);
+
+categoriesItem.map((elem) => {
+  const categoriesTitle = elem.querySelector("h2");
+  console.log(`Category: ${categoriesTitle.textContent}`);
+
+  const categoryLength = elem.querySelectorAll("li");
+  console.log(`Element: ${categoryLength.length}`);
+});
