5346d3b24226858d88e2dac3c8c4e3dabf21b517
maryna-haliieva <marina.galieva7@gmail.com>
2022-07-07 12:44:40 +0300

    add task 02

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,27 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const list = document.querySelector("#ingredients");
+console.log(list);
+
+const newArr = (array) => {
+  return array.map((elem) => {
+    const itemList = document.createElement("li");
+
+    itemList.textContent = elem;
+    console.log(itemList);
+
+    itemList.classList.add("item");
+
+    return itemList;
+  });
+};
+
+const addIngridients = newArr(ingredients);
+list.append(...addIngridients);
+console.log(newArr);
