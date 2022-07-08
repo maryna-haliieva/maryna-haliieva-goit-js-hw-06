86d2437093310e0d3c13d024e3f392fe12d1da7f
maryna-haliieva <marina.galieva7@gmail.com>
2022-07-08 14:09:52 +0300

    add task 4

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,44 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+function galleryMarkup() {
+  const cardList = document.querySelector(".gallery");
+
+  const cardsItem = images
+    .map(
+      (elem) =>
+        `<li> <img class ="img" src ="${elem.url}" alt = "${elem.alt}"  /></li>`
+    )
+    .join(" ");
+
+  cardList.insertAdjacentHTML("afterbegin", cardsItem);
+  console.log(cardList);
+
+  cardList.style.display = "flex";
+  cardList.style.justifyContent = "center";
+  cardList.style.flexGrow = "1";
+  cardList.style.listStyle = "none";
+  cardList.style.padding = "0";
+
+  const imgMarkup = document.querySelectorAll(".img");
+  imgMarkup.forEach(
+    (elem) => (elem.style.height = "100%") && (elem.style.width = "100%")
+  );
+
+  const itemMarkup = document.querySelectorAll("li");
+  itemMarkup.forEach((elem) => (elem.style.margin = "15px"));
+}
+
+galleryMarkup();
---
+++ js/task-04.js
@@ -0,0 +1,25 @@
+const decrementButton = document.querySelector('[data-action ="decrement"]');
+console.log(decrementButton);
+const incrementButton = document.querySelector('[data-action ="increment"]');
+console.log(incrementButton);
+const value = document.querySelector("#value");
+console.log(value);
+
+let counterValue = 0;
+
+function initialValue() {
+  value.textContent = counterValue;
+}
+
+decrementButton.addEventListener("click", onClickDecrementButton);
+
+function onClickDecrementButton() {
+  counterValue -= 1;
+  initialValue();
+}
+
+incrementButton.addEventListener("click", onClickIncrementButton);
+function onClickIncrementButton() {
+  counterValue += 1;
+  initialValue();
+}
