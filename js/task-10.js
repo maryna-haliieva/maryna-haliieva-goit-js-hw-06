4422f1afe916c445495913eb098589f182c4dcac
maryna-haliieva <marina.galieva7@gmail.com>
2022-07-12 19:32:46 +0300

    add task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const [input, createBtn, destroyBtn] =
+  document.querySelector("#controls").children;
+const boxes = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", onCreate);
+function onCreate() {
+  const arr = [];
+  for (let index = 1; index <= +input.value; index++) {
+    const wrapper = document.createElement("div");
+    wrapper.style.height = wrapper.style.width = `${30 + index * 10}px`;
+    wrapper.style.backgroundColor = getRandomHexColor();
+    arr.push(wrapper);
+  }
+  boxes.append(...arr);
+}
+
+destroyBtn.addEventListener("click", onDestroy);
+function onDestroy() {
+  boxes.innerHTML = "";
+  input.value = "";
+}
